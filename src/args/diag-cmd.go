c748fad79ac915726adfea5afd7728da7944babc
Vinay Venkatesh <>
2024-01-12 12:59:55 -0500

    Updated colors and minor fix.

--- src/args/diag-cmd.go
+++ src/args/diag-cmd.go
@@ -135,8 +135,8 @@ func getADM(file string) *model.Model {
 func getConfig() graphviz.GraphvizConfig {
 	return graphviz.GraphvizConfig{
 		Assumption: graphviz.NodeProperties{
-			Color: graphviz.ColorSet{FontColor: "white", FillColor: "dimgray", BorderColor: "dimgray"},
-			Font:  graphviz.TextProperties{FontName: "Times", FontSize: "18"},
+			Color: graphviz.ColorSet{FontColor: "brown", FillColor: "", BorderColor: "lightgray"},
+			Font:  graphviz.TextProperties{FontName: "Arial", FontSize: "14"},
 		},
 		Policy: graphviz.NodeProperties{
 			Color: graphviz.ColorSet{FontColor: "black", FillColor: "darkolivegreen3", BorderColor: "darkolivegreen3"},
@@ -168,6 +168,10 @@ func getConfig() graphviz.GraphvizConfig {
 		},
 		EmptyAttack: graphviz.NodeProperties{
 			Color: graphviz.ColorSet{FontColor: "black", FillColor: "transparent", BorderColor: "red"},
+			Font:  graphviz.TextProperties{FontName: "Arial", FontSize: "16"},
+		},
+		UnMitigatedAttack: graphviz.NodeProperties{
+			Color: graphviz.ColorSet{FontColor: "red", FillColor: "yellow", BorderColor: "red"},
 			Font:  graphviz.TextProperties{FontName: "Arial", FontSize: "16"},
 		},
 
